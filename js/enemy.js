5aef359b1ce59a6427993ee42e419858845c0558
SkyCrystal <plzcompromise@gmail.com>
2019-01-22 23:10:28 -0700

    OOP Design

--- js/enemy.js
+++ js/enemy.js
@@ -1,4 +1,8 @@
-class Enemies{
+class Enemy{
+    constructor(){
+        this.x = 100;
+        this.y = 100;
+    }
 
     create(x,y,width,height){
         this.x = x;
@@ -7,5 +11,20 @@ class Enemies{
         Engine.createRect(this.x, this.y, width, height);
         this.velX = 5.0;
         this.velY = 5.0;
+    }
+
+    update(one_velX, one_velY, canvas){
+            this.velX = one_velX;
+            this.velY = one_velY;
+            this.x += this.velX;
+            this.y += this.velY;
+    
+            if((this.y <= 0) || (this.y >= canvas.height - 50)){
+                    this.velY *= -1.0;
+            }
+    
+            if((this.x <= 0) || (this.x >= canvas.width - 50)){
+                    this.velX *= -1.0;
+            }
     }
 }
